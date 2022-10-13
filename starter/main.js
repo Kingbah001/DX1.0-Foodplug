3ca35447f3609a91856805d4eb6463430d6425f5
Kingbah001 <114989677+Kingbah001@users.noreply.github.com>
2022-10-13 05:49:12 -0700

    starting the javascript

---
+++ starter/main.js
@@ -0,0 +1,15 @@
+
+
+
+//ES5
+// console.log("send me your account number")
+// var writeup = "i'm hungry"
+// alert(writeup)
+// console.log(hamburgerdiv)
+// console.log(document)
+
+var hamburgerdiv = document.querySelector(".hamburger")
+
+function dosomething (){
+    hamburgerdiv.classList.toggle("showburger")
+}
